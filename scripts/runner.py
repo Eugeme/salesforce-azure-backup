91359eda6dc4b18d644e77948c7a1dfbf4795cc4
Eugeme <eugenemalashenkoo@gmail.com>
2023-04-14 21:56:13 +0200

    fix salesforce_ids script

--- scripts/runner.py
+++ scripts/runner.py
@@ -1,3 +1,10 @@
+from utils import connect_salesforce
 import salesforce_ids
 
-salesforce_ids = salesforce_ids.get_ids()
+
+sf = connect_salesforce.connect()
+table_names = sf.query_all("SELECT SObjectType FROM ObjectPermissions GROUP BY SObjectType")
+table_names = [i['SobjectType'] for i in table_names['records']]
+
+for table_name in table_names:
+    ids = salesforce_ids.get_ids(sf, table_name)
--- scripts/salesforce_ids.py
+++ scripts/salesforce_ids.py
@@ -1,23 +1,16 @@
-from utils import connect_salesforce
 from simple_salesforce import exceptions
 
 
-def get_ids():
-    sf = connect_salesforce.connect()
+def get_ids(sf, table_name):
+    try:
+        ids = sf.query_all(f'SELECT Id FROM {table_name}')
 
-    table_names = sf.query_all('SELECT SObjectType FROM ObjectPermissions GROUP BY SObjectType')
-    table_names = [i['SobjectType'] for i in table_names['records']]
-
-    all_ids = []
-
-    for table_name in table_names:
-        try:
-            ids = sf.query_all(f'SELECT Id FROM {table_name}')
-        except exceptions.SalesforceMalformedRequest:
-            continue
-
-        all_ids += [i['Id'] for i in ids['records']]
+        ids = [i['Id'] for i in ids['records']]
 
         print(f'records in {table_name}: {str(len(ids))}')
 
-    return all_ids
+        return ids
+
+    except exceptions.SalesforceMalformedRequest as e:
+        print(e)
+        pass
